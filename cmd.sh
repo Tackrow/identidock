ca4d41f9c9428582adab04e1f27bb473d28f4f45
Takuro Morioka <mt1028r@gmail.com>
2020-04-11 08:55:57 +0900

    add shell

--- cmd.sh
+++ cmd.sh
@@ -7,6 +7,11 @@ set -e
 if [ "$ENV" = 'DEV' ]; then
     echo "Running Development Server"
     exec python "identidock.py"
+
+elif [ "$ENV" = 'UNIT' ]; then
+    echo "Running Development Server"
+    exec python "test.py"
+
 else
     echo "Running Production Server"
     exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py --callable app --stats 0.0.0.0:9191
